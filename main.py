855683d95f724f65f11c565543cfd6b765e824ad
mixalych <mixalych06@mail.ru>
2024-03-10 01:37:26 +0900

    html templates have been created video.html , docs.html

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 
+from utils.pars_content import parsing_path_doc, parsing_path_video
 
 app = Flask(__name__)
 
@@ -8,23 +9,30 @@ app = Flask(__name__)
 def main_page():
     return 'Hello'
 
+
 @app.route('/video')
-def main_page():
-    return 'Hello'
+def video_page():
+    contents = parsing_path_video()
+    print(contents)
+    return render_template('video.html', video_title='Видео', contents=contents)
+
+
 
 @app.route('/mimo')
-def main_page():
-    return 'Hello'
+def mimo_page():
+    contents = parsing_path_doc()
+    print(contents)
+    return render_template('docs.html', docs_title='Памятки', contents=contents)
+
 
 @app.route('/manual')
-def main_page():
+def manual_page():
     return 'Hello'
-
-@app.route('/author')
-def main_page():
-    return 'Hello'
-
-
+#
+#
+# @app.route('/author')
+# def main_page():
+#     return 'Hello'
 
 
 app.run(debug=True)
--- utils/pars_content.py
+++ utils/pars_content.py
@@ -5,21 +5,17 @@ path_doc = '../static/content/Памятки'
 
 
 def parsing_path_video():
-    path_video = '../static/content/Видео'
+    path_video = 'static/content/Видео'
     rez = glob.glob(path_video + '/*/*.mp4')
     rez.extend(glob.glob(path_video + '/*/*.avi'))
     print(rez)
-    for n, item in enumerate(sorted(rez)):
-        print(n + 1, item)
+    return sorted(rez)
 
 
 def parsing_path_doc():
-    path_document = '../static/content/Памятки'
+    path_document = 'static/content/Памятки'
     # path_video = '../static/content/Видео'
     # rez = glob.glob(path_video + '/*/*.docx')
-    rez = (glob.glob(path_document + '/**.docx'))
-    for n, item in enumerate(sorted(rez)):
-        print(n + 1, item)
-
-
-parsing_path_doc()
+    rez = (glob.glob(path_document + '/*.docx'))
+    print(rez)
+    return sorted(rez)
